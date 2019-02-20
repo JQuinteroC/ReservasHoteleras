5690b91a89a7a2272259fd5edccd823dac3add33
estudiantes <estudiantes@labing50324.udistrital.edu.co>
2019-02-20 13:37:58 -0500

    agrego el JScrollPane dentro de un Jtable para luego poner una tabla de la bd ( es la plantilla)

--- ReservasHoteleras/src/GUI/Reserva.java
+++ ReservasHoteleras/src/GUI/Reserva.java
@@ -9,13 +9,11 @@ import LOGIC.FormatoCalendar;
 import com.toedter.calendar.JDateChooser;
 import java.awt.Container;
 import java.awt.Font;
-import java.awt.HeadlessException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 
 /**
@@ -33,6 +31,8 @@ public class Reserva extends JFrame{
     JDateChooser jd = new JDateChooser();
     JTextField txtObtDate = new JTextField();
     FormatoCalendar f = new FormatoCalendar();
+    JTable tblHab = new  JTable();
+    JScrollPane scroll = new JScrollPane(tblHab);
     
 
     public Reserva(){
@@ -50,6 +50,7 @@ public class Reserva extends JFrame{
         c.add(btnVol);
         c.add(jd);
         c.add(txtObtDate);
+        c.add(scroll);
         
         lblFecha.setFont(new Font("dialog", 1, 24));
         lblNoches.setFont(new Font("dialog", 1, 24));
@@ -57,6 +58,19 @@ public class Reserva extends JFrame{
         lblFecha.setBounds(45, 75, 250, 25);
         lblNoches.setBounds(350, 75, 350, 25);
         lblHabDis.setBounds(225, 250, 350, 25);
+        //Link de una manera de poner una Tabla de la db a un JScrollPane
+        tblHab.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+            },
+            new String [] {
+                "*","Nombre", "Número", "Tipo", "Costo noche", "Capacidad"
+            }
+        ));
+        scroll.setBounds(180, 310, 500, 250);
         textCntNoc.setBounds(350, 105, 100, 25);
         btnVeri.setBounds(190, 180, 150, 35);
         btnVeri.addActionListener(new java.awt.event.ActionListener() {
@@ -64,13 +78,13 @@ public class Reserva extends JFrame{
                 btnVeriActionPerformed(evt);
             }
         });
-        btnRes.setBounds(320, 525, 150, 35);
+        btnRes.setBounds(320, 805, 150, 35);
         btnRes.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnResActionPerformed(evt);
             }
         });
-        btnVol.setBounds(520, 525, 150, 35);
+        btnVol.setBounds(520, 805, 150, 35);
         btnVol.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnVolActionPerformed(evt);
@@ -80,7 +94,7 @@ public class Reserva extends JFrame{
         txtObtDate.setBounds(10, 150, 150, 35);
         
         setVisible(true);
-        setSize(800,700);
+        setSize(800,900);
     }
     
     public void btnVeriActionPerformed(java.awt.event.ActionEvent evt) {
